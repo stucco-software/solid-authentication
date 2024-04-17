00110dc2e425b6e96df83285b43d391704978885
nikolaswise <nikolaswise@gmail.com>
2024-04-17 10:33:31 +0200

    no …

--- dpop.js
+++ dpop.js
@@ -63,8 +63,8 @@ export const createDPoP = async ({code, iss}) => {
 
   const token_endpoint = sessionStorage.getItem('token_endpoint')
   const tokenID = crypto.randomUUID()
-  // const tokenCreated = new Date().getTime()
-  const tokenCreated = Math.floor(Date.now() / 1000)
+  const tokenCreated = new Date().getTime()
+  // const tokenCreated = Math.floor(Date.now() / 1000)
   const tokenHeader = {
     "alg": "ES256",
     "typ": "dpop+jwt",
